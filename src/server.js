feca58fbf9e9027d425fa97f3454421659199fd6
syamcat <kdh1998kdh@gmail.com>
2024-08-04 22:36:08 +0900

    add request get method

--- src/server.js
+++ src/server.js
@@ -3,6 +3,12 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views"); // /src 가 root로 되어있음
 
-app.listen(3000); 
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+const handleListen = () => console.log(`Listening on http://localhost:3000`) // console.log를 찍는 함수를 저장
+
+app.listen(3000, handleListen); 
