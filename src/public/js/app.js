6e79e58b0dddc547c868af4b96119d33dad66f3c
syamcat <kdh1998kdh@gmail.com>
2024-08-27 08:36:23 +0900

    make room

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,9 +1,24 @@
 const socket = io();
 const welcome = document.getElementById("welcome");
+const room = document.getElementById("room");
 const form = welcome.querySelector("form");
 
-function backendDone(message) {
-	console.log(`message: `, message);
+room.hidden = true;
+
+let roomName;
+
+function addMessage(message) {
+	const ul = room.querySelector("ul");
+	const li = document.createElement("li");
+	li.innerText = message;
+	ul.appendChild(li);
+}
+
+function showRoom() {
+	welcome.hidden = true;
+	room.hidden = false;
+	const h3 = room.querySelector("h3");
+	h3.innerText = `Room ${roomName}`;
 }
 
 function handleRoomSubmit(event) {
@@ -13,12 +28,18 @@ function handleRoomSubmit(event) {
 	// socket.emit("enter_room", { payload: input.value });
 
 	// socket.io를 이용하면 text외에 다른 형식도 보낼 수 있다.
-	socket.emit("enter_room", { payload: input.value},
+	/* socket.emit(eventName, param1, param2, ..., function); 이런 형태로 작성 */
+	socket.emit("enter_room", { payload: input.value },
 		// 함수도 전송 가능
 		// 서버에서 실행하면 프론트에서 작동한다!!!
-		backendDone
+		showRoom
 	);
+	roomName = input.value;
 	input.value = "";	// 입력값 초기화
 }
 
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", () => {
+	addMessage("someone joined!");
+});
--- src/server.js
+++ src/server.js
@@ -20,11 +20,21 @@ const wsServer = new Server(httpServer);	// Socket.io 서버 생성
 
 // connection 감지
 wsServer.on("connection", socket => {
+	socket.onAny((event) => {
+		console.log(`Socket Event:${event}`);
+	});
 	/* enter_room이라는 event수신 socket.on(event이름, (인자1, 인자2, ...) => {실행내용} ) */
-	socket.on("enter_room", (msg, backendDone) => {
-		console.log(msg);	// message 출력
+	socket.on("enter_room", (roomName, backendDone) => {
+		console.log(socket.id);	// Client의 id
+		console.log(socket.rooms);	// socket.rooms: socket이 참여중인 room 출력
+		socket.join(roomName.payload);	// room에 참가 / socket.leave(roomName)로  나가는 것도 가능
+		console.log(socket.rooms);
+		backendDone();
+		// socket.to()라는 함수도 있다.
+		socket.to(roomName.payload).emit("welcome");	// 해당 room의 모든 사람에게 이벤트 보냄
+
 		// backend에서 실행시키지만 실행은 Frontend에서 진행된다!
-		backendDone("backend is done");
+		// backendDone("backend is done");	// Front 함수에 메세지를 담아서 실행
 	});
 })
 
