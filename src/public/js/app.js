a1633a0fb1a87477ed29be2389c69b6a9c8ce391
syamcat <kdh1998kdh@gmail.com>
2024-08-30 16:22:23 +0900

    webRTC server

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,73 +1,138 @@
 const socket = io();
-const welcome = document.getElementById("welcome");
-const room = document.getElementById("room");
-const form = welcome.querySelector("form");
+const myFace = document.getElementById("myFace");	// 비디오 태그
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
+const call = document.getElementById("call");
 
-room.hidden = true;
+call.hidden = true;
 
+let myStream;
+let muted = false;
+let cameraOff = false;
 let roomName;
+let myPeerConnection;
 
-function addMessage(message) {
-	const ul = room.querySelector("ul");
-	const li = document.createElement("li");
-	li.innerText = message;
-	ul.appendChild(li);
+async function getCameras() {
+	try {
+		// 입출력 장치들의 정보를 전부 불러옴
+		const devices = await navigator.mediaDevices.enumerateDevices();
+		// 불러온 장치들 중 비디오 input 장치들의 정보만 가져옴
+		const cameras = devices.filter(device => device.kind === "videoinput");
+		const currentCamera = myStream.getVideoTracks()[0];
+		// console.log(cameras);
+		cameras.forEach(camera => {
+			const option = document.createElement("option");	// option 태그 생성
+			option.value = camera.deviceId;	// value옵션에는 카메라 id
+			option.innerText = camera.label;	// 출력되는 텍스트는 카메라 이름을 넣어준다.
+			if (currentCamera.label === camera.label) {	// 현재 촬영중인 카메라와 option에 추가하려는 camera의 label이 같으면
+				option.selected = true;	// 옵션에서 선택된 카메라로 보여줌
+			}
+			camerasSelect.appendChild(option);	//옵션을 Select 태그 사이에 추가
+		});
+	} catch (e) {
+		console.log(e);
+	}
 }
 
-function handleMessageSubmit(event) {
-	event.preventDefault();
-	const input = room.querySelector("#msg input");	// msg form에서 input요소 가져오기
-	const value = input.value;	// 시작시 미리 저장해두는 이 명령줄이 없다면
-	socket.emit("new_message", input.value, roomName, () => {
-		addMessage(`You: ${value}`);
-	});
-	input.value = "";	// 명령문이 순서대로 실행되지 않기 때문에 addMessage(`You: 이부분이 출력 안됨`)
+async function getMedia(deviceId) {
+	const initialConstrains = {
+		audio: true,
+		video: { facingMode: "user" }
+	};
+	const cameraConstrains = {
+		audio: true,
+		// exact를 없이 deviceId를 작성하면 deviceId를 못 찾았을 때 다른 카메라로 출력해준다.
+		video: { deviceId: { exact: deviceId } },
+	}
+
+	try {
+		myStream = await navigator.mediaDevices.getUserMedia(	// 디바이스 Input가져옴
+			deviceId ? cameraConstrains : initialConstrains	// deviceId 없은 처음 경우 예외처리
+		);
+		myFace.srcObject = myStream;	// 비디오 플레이어에 영상 스트림 입력
+		if (!deviceId) {
+			await getCameras();	// 처음에만 카메라 목록 불러오기 + 추가
+		}
+		// console.log(myStream);
+	} catch (e) {
+		console.log(e);
+	}
+};
+
+function handleMuteClick() {
+	// getAudioTracks의 속성 값 중에서 enabled(활성화 여부)를 가져와 토글하는 명령
+	myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
+	if (!muted) {
+		muteBtn.innerText = "Unmute";
+		muted = true;
+	} else {
+		muteBtn.innerText = "Mute";
+		muted = false;
+	}
 }
 
-function handleNicknameSubmit(event) {
-	event.preventDefault();
-	const input = room.querySelector("#name input");	// name form에서 input요소 가져오기
-	const value = input.value;	// 시작시 미리 저장해두는 이 명령줄이 없다면
-	socket.emit("nickname", input.value);
-	input.value = "";	// 명령문이 순서대로 실행되지 않기 때문에 addMessage(`You: 이부분이 출력 안됨`)
+function handleCameraClick() {
+	myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
+	if (cameraOff) {
+		cameraBtn.innerText = "Turn Camera Off";
+		cameraOff = false;
+	} else {
+		cameraBtn.innerText = "Turn Camera On";
+		cameraOff = true;
+	}
 }
 
-function showRoom() {
+async function handleCameraChange() {
+	// console.log(cameraSelec.value);
+	await getMedia(camerasSelect.value);
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
+
+// Welcom form (join a room)
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+async function startMedia() {
 	welcome.hidden = true;
-	room.hidden = false;
-	const h3 = room.querySelector("h3");
-	h3.innerText = `Room ${roomName}`;
-	const msgForm = room.querySelector("#msg");
-	const nameForm = room.querySelector("#name");
-	msgForm.addEventListener("submit", handleMessageSubmit);
-	nameForm.addEventListener("submit", handleNicknameSubmit);
+	call.hidden = false;
+	await getMedia();
+	makeConnection();
 }
 
-function handleRoomSubmit(event) {
+function handleWelcomeSubmit(event) {
 	event.preventDefault();
-	const input = form.querySelector("input");	// input 태그 정보 가져옴
-	/* enter_room 이라는 event 이름에 {payload: input태그의 value값} 객체를 담아서 전송 */
-	// socket.emit("enter_room", { payload: input.value });
+	const input = welcomeForm.querySelector("input");
 
-	// socket.io를 이용하면 text외에 다른 형식도 보낼 수 있다.
-	/* socket.emit(eventName, param1, param2, ..., function); 이런 형태로 작성 */
-	socket.emit("enter_room", { payload: input.value },
-		// 함수도 전송 가능
-		// 서버에서 실행하면 프론트에서 작동한다!!!
-		showRoom
-	);
+	// emit: 함수를 함수명() 이런식으로 넣으면  함수명만 넣어야 한다.
+	socket.emit("join_room", input.value, startMedia);
 	roomName = input.value;
-	input.value = "";	// 입력값 초기화
+	input.value = "";
+	// console.log(input.value);
 }
 
-form.addEventListener("submit", handleRoomSubmit);
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
-socket.on("welcome", (user) => {
-	addMessage(`${user} joined!`);
+// Socket Code
+socket.on("welcome", async () => {
+	// Session 정보가 담겨있음
+	const offer = await myPeerConnection.createOffer();
+	myPeerConnection.setLocalDescription(offer);
+	console.log("sent the offer");
+	socket.emit("offer", offer, roomName);
 });
 
-socket.on("bye", (user) => {
-	addMessage(`${user} left`);
+socket.on("offer", (offer) => {
+	console.log(offer);
 });
 
-socket.on("new_message", addMessage);
+// RTC Code
+function makeConnection() {
+	myPeerConnection = new RTCPeerConnection();
+	myStream
+		.getTracks()
+		.forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ import { Server } from "socket.io"
 import http from "http";
 import { Collection } from "mongoose";
 
-
 const app = express();
 
 app.set("view engine", "pug");
@@ -18,40 +17,24 @@ const handleListen = () => console.log(`Listening on ws://localhost:3000`); // c
 const httpServer = http.createServer(app);	// http 서버 생성
 const wsServer = new Server(httpServer);	// Socket.io 서버 생성
 
-// connection 감지
 wsServer.on("connection", socket => {
-
-	socket["nickname"] = "Anon";	// Default nickname Anon
-
 	// 들어오는 모든 이벤트를 출력해준다.
 	socket.onAny((event) => {
+		console.log(wsServer.sockets.adapter); // adapter: 서버간의 실시간 동기화를 위해 사용
 		console.log(`Socket Event:${event}`);
 	});
-	/* enter_room이라는 event수신 socket.on(event이름, (인자1, 인자2, ...) => {실행내용} ) */
-	socket.on("enter_room", (roomName, backendDone) => {
-		console.log(socket.id);	// Client의 id
-		console.log(socket.rooms);	// socket.rooms: socket이 참여중인 room 출력
-		socket.join(roomName.payload);	// room에 참가 / socket.leave(roomName)로  나가는 것도 가능
-		console.log(socket.rooms);
-		backendDone();
-		// socket.to()라는 함수도 있다.
-		socket.to(roomName.payload).emit("welcome", socket.nickname);	// 해당 room의 모든 사람에게 이벤트 보냄
 
-		// backend에서 실행시키지만 실행은 Frontend에서 진행된다!
-		// backendDone("backend is done");	// Front 함수에 메세지를 담아서 실행
-	});
-	// 접속 종료(브라우저 종료, 인터넷 중단) 이벤트
-	socket.on("disconnecting", () => {
-		socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname)); // 모든 방에 알림
-	});
-	// 새로운 메세지가 입력됨
-	socket.on("new_message", (msg, room, done) => {
-		socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-		done();
+	socket.on("join_room", (roomName, startMedia) => {
+		console.log("here is join_room(roomName):", roomName);
+		socket.join(roomName);
+		// console.log("here is join_room(startMedia):", startMedia); 
+		startMedia();
+		socket.to(roomName).emit("welcome"); 
 	});
 
-	socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+	socket.on("offer", (offer, roomName) => {
+		socket.to(roomName).emit("offer", offer);
+	});
 });
-
 
 httpServer.listen(3000, handleListen);	// 3000번 포트 이벤트 리스너 생성
