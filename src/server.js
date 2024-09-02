4c6021e5e923997b3b5fc6f14f2c08ebf5e957d7
MSI\Admin <kdh1998kdh@gmail.com>
2024-09-02 19:02:55 +0900

    webRTC apply https

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 import express from "express";
 import { Server } from "socket.io"
 import http from "http";
-import { Collection } from "mongoose";
+import https from "https";
+import fs from "fs";
+import path from "path";
 
 const app = express();
+
+// const sslCertFile = process.env.SSL_CRT_FILE || "cert/localhost.pem";
+// const sslKeyFile = process.env.SSL_KEY_FILE || "cert/localhost-key.pem";
+
+// const certPath = path.resolve(__dirname + "cert/localhost+2.pem")
+// const keyPath = path.resolve(__dirname, "cert/localhost+2-key.pem")
+
+
+const sslOptions = {
+	key: fs.readFileSync(__dirname + "/../cert/localhost+2-key.pem", "utf-8"),
+	cert: fs.readFileSync(__dirname + "/../cert/localhost+2.pem", "utf-8")
+}
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views"); // /src 가 root로 되어있음
@@ -14,7 +28,7 @@ const handleListen = () => console.log(`Listening on ws://localhost:3000`); // c
 
 // app.listen(3000, handleListen); //3000번 포트로 이벤트 리스너 열기, listen(포트번호, 이벤트 처리 함수)
 
-const httpServer = http.createServer(app);	// http 서버 생성
+const httpServer = https.createServer(sslOptions, app);	// http 서버 생성
 const wsServer = new Server(httpServer);	// Socket.io 서버 생성
 
 wsServer.on("connection", socket => {
